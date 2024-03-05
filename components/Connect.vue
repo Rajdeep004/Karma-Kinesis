bb8e7bfc47cfd23a5eea221db2d03ee5990a68f9
Rajdeep <snnbarot@gmail.com>
2024-03-05 12:42:32 +0530

    fix: Minor change

--- components/Connect.vue
+++ components/Connect.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <section id="connect" class="container mx-auto  border-t-2 mt-11  flex items-center md:p-10">
+    <section id="connect" class="container mx-auto border-t-2 mt-11  flex items-center justify-center md:p-10">
         <form action="#" class="w-full flex p-7 flex-col gap-4 md:w-3/4 h-1/2">
             <div class="flex flex-col gap-4">
                 <h1 class="text-3xl font-medium">Sign up for our Classes</h1>
@@ -21,9 +21,9 @@
             </div>
             <div class="flex">
                 <textarea name="" id="" cols="30" rows="10" placeholder="Message" class="p-5 rounded-3xl border-2 border-gray-300 w-full resize-none lg:w-3/4"></textarea>
-                <div class="hidden lg:flex justify-center items-center -ml-8"><Icon name="system-uicons:arrow-up" class="bg-gradient-to-br from-[#f1a879] to-[#e97163] text-6xl p-4 rounded-full"/></div>
+                <button class="hidden lg:flex justify-center items-center -ml-8"><Icon name="system-uicons:arrow-up" class="bg-gradient-t-br bg-[#3CB333] from-[#f1a879] to-[#e97163] text-6xl p-4 text-white rounded-full cursor-pointer"/></button>
             </div>
-            <button class="bg-gradient-to-br from-[#f1a879] to-[#e97163] flex justify-center py-3 rounded-3xl font-bold text-xl lg:w-1/4">GET STARTED</button>
+            <button class="bg-gradient-t-br bg-[#3CB333] text-white from-[#f1a879] to-[#e97163] flex justify-center py-3 rounded-3xl font-bold text-xl lg:w-1/4">GET STARTED</button>
         </form>
     </section>
 </template>
